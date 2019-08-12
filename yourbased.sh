67f6af38c4cbde15b661c0ed01e6ea736f7386b5
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-12 17:40:17 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -3,6 +3,6 @@ set -ex
 
 
 test/ci/before_install
-bundle install --jobs=3 --retry=3 --full-index --without development
+bundle install --full-index --without development
 test/ci/before_script
 xvfb-run bundle exec rake test:models test:libs test:controllers DRIVER=webkit
