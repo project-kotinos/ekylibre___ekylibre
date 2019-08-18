f76887a2911e5ec7be69eb7e62afc2cf560e57fc
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-18 14:02:42 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
+export DEBIAN_FRONTEND=noninteractive
+export CI=true
+export TRAVIS=true
+export CONTINUOUS_INTEGRATION=true
+export USER=travis
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RAILS_ENV=test
+export RACK_ENV=test
+export MERB_ENV=test
+export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
 
 test/ci/before_install
 bundle install --full-index --without development
